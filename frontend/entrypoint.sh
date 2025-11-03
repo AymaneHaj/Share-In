e4b26321d48f1b253c0e6ea12a03106ed8ffdd84
AymaneHaj <happyshop120@gmail.com>
2025-11-04 00:32:31 +0100

    updates

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,2 +1,2 @@
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${PORT},${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
